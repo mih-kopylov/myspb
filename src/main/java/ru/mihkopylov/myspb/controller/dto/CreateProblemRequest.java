55c37ce289e5246bdeb7e3766f1ac76385aa40de
Mikhail Kopylov <mih.kopylov@yandex.ru>
2021-07-26 16:53:32 +0300

    Add human readable error messages for validation

--- src/main/java/ru/mihkopylov/myspb/controller/dto/CreateProblemRequest.java
+++ src/main/java/ru/mihkopylov/myspb/controller/dto/CreateProblemRequest.java
@@ -9,12 +9,12 @@ import org.springframework.web.multipart.MultipartFile;
 @Getter
 @Setter
 public class CreateProblemRequest {
-    @NotNull
+    @NotNull(message = "Нужно указать причину обращения")
     private Long reasonGroupId;
-    @NotNull
+    @NotNull(message = "Координаты: широта обязательна")
     private Double latitude;
-    @NotNull
+    @NotNull(message = "Координаты: долгота обязательна")
     private Double longitude;
-    @NotEmpty
+    @NotEmpty(message = "Должен быть как минимум один файл")
     private MultipartFile[] files;
 }
